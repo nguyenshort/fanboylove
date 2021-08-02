2861bded2b428e3d0510ac65c803ba0f4438e902
dnstylish <Khoi@025>
2021-08-02 07:32:45 +0700

    add scheduler chapter

--- vue/components/settings/TabBar.vue
+++ vue/components/settings/TabBar.vue
@@ -1,42 +1,78 @@
 <template>
   <div class="nav-tabs-wrap">
     <ul class="nav nav-tabs">
-      <router-link to="/settings/bookmark" custom v-slot="{ navigate, isActive }">
+      <router-link
+        v-if="false"
+        v-slot="{ navigate, isActive }"
+        to="/settings/bookmark"
+        custom
+      >
         <li :class="isActive ? 'active' : ''">
-          <a @click="navigate" @keypress.enter="navigate" href="javascript:void(0)">
-            <i class="icon ion-ios-bookmark"></i>
+          <a
+            href="javascript:void(0)"
+            @click="navigate"
+            @keypress.enter="navigate"
+          >
+            <i class="icon ion-ios-bookmark" />
             Theo Dõi
           </a>
         </li>
       </router-link>
 
+      <li class="">
+        <a href="javascript:void(0)"
+          ><i class="icon ion-ios-bookmark" />
+          Theo Dõi
+        </a>
+      </li>
 
-
-      <router-link to="/settings/history" custom v-slot="{ navigate, isActive }">
+      <router-link
+        v-if="false"
+        v-slot="{ navigate, isActive }"
+        to="/settings/history"
+        custom
+      >
         <li :class="isActive ? 'active' : ''">
-          <a @click="navigate" @keypress.enter="navigate" href="javascript:void(0)">
-            <i class="icon ion-md-alarm"></i>
+          <a
+            href="javascript:void(0)"
+            @click="navigate"
+            @keypress.enter="navigate"
+          >
+            <i class="icon ion-md-alarm" />
             Lịch Sử
           </a>
         </li>
       </router-link>
 
-      <router-link to="/settings/account" custom v-slot="{ navigate, isActive }">
+      <li class="">
+        <a href="javascript:void(0)"
+          ><i class="icon ion-md-alarm" />
+          Lịch Sử
+        </a>
+      </li>
+
+      <router-link
+        v-slot="{ navigate, isActive }"
+        to="/settings/account"
+        custom
+      >
         <li :class="isActive ? 'active' : ''">
-          <a @click="navigate" @keypress.enter="navigate" href="javascript:void(0)">
-            <i class="icon ion-md-person"></i>
+          <a
+            href="javascript:void(0)"
+            @click="navigate"
+            @keypress.enter="navigate"
+          >
+            <i class="icon ion-md-person" />
             Tài Khoản
           </a>
         </li>
       </router-link>
-
-
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TabBar"
+  name: 'TabBar'
 }
 </script>
