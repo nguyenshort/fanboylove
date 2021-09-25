fc79544310aa1abb03607966eefd57b70b16f95e
yuan <dnstylish@gmail.com>
2021-09-25 17:31:08 +0700

    fix bug

--- commands/fixNettruyen.js
+++ commands/fixNettruyen.js
@@ -2,19 +2,30 @@ require('dotenv').config({ path: '../.env' })
 const database = require('../database')
 database.connect()
 
-const Chapter = require('../models/Chapter')
 const Story = require('../models/Story')
+
+const netTruyen = require('../modules/crawl/site/nettruyen')
 
 async function f() {
   const stories = await Story.find({
     source: {
-      $regex: /http:\/\/www.nettruyenvip.com/
+      $regex: /http:\/\/www.nettruyenpro.com/
     }
   })
   for (const story of stories) {
-    await Chapter.findOneAndDelete({ story: story._id })
-    await Story.findByIdAndDelete(story._id)
+    const Nettruyen = new netTruyen(story.source)
+    await Nettruyen.init()
+    const link = Nettruyen.Leech.getAttr('.col-image img', 'src').single()
+    const avatar = await Nettruyen.Leech.downloadAvatar(
+      /^(http|https)/.test(link) ? link : 'http:' + value,
+      {
+        Referer: 'http://www.nettruyenpro.com/'
+      }
+    )
+    Story.findByIdAndDelete(story._id)
+    console.log('Done ', avatar)
   }
+  console.log('Done')
 }
 
 f()
